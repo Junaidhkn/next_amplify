31ec763f0373e075727fe92d4cb1647444366fd8
junaidhkn <junaidhkniazi@gmail.com>
2023-10-29 03:49:37 +0500

    layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { Amplify } from 'aws-amplify';
+import awsExports from '@/aws-exports';
+import '@aws-amplify/ui-react/styles.css';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+
+Amplify.configure({ ...awsExports, ssr: true });
+
+function App({ Component, pageProps }: AppProps) {
+	return <Component {...pageProps} />;
 }
+
+export default withAuthenticator(App);
