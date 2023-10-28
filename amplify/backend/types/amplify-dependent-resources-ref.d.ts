eb98efb79718f46cf00164e06be9c93ed9dbc031
junaidhkn <junaidhkniazi@gmail.com>
2023-10-28 20:32:37 +0500

    amplify pull & update codegen

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "dashboardproducts": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "dashboardproducts": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3dashboardrewerewrer": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
